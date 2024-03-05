89b4ed52c3370893ae5b3d91276283911f28d540
Luci-Park <97658764+Luci-Park@users.noreply.github.com>
2024-03-05 09:04:47 +0900

    [Gold IV] Title: 부분합, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Gold/1806. 부분합/부분합.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int numbers[100000];
+int sol(int n, long s)
+{
+	long sum = 0;
+	for (size_t i = 0; i < n; i++)
+	{
+		cin >> numbers[i];
+		if (numbers[i] >= s) return 1;
+		sum += numbers[i];
+	}
+	if (sum < s) return 0;
+	int answer = n;
+	sum = 0;
+	int left = 0, right = 0;
+	while (left < n)
+	{
+		if (sum < s)
+		{
+			if (right <= n - 1) sum += numbers[right++];
+			else break;
+		}
+		else
+		{
+			int length = right - left;
+			if (length < answer) answer = length;
+			sum -= numbers[left++];
+		}
+	}
+	return answer;
+}
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int n;
+	long s;
+	cin >> n >> s;
+	cout << sol(n, s);
+	return 0;
+}
