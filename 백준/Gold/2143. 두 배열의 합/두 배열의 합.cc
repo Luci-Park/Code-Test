3cd622a828b0de64979eda7fc6d2752a2438c150
Sumin Park <97658764+Luci-Park@users.noreply.github.com>
2025-01-18 17:20:47 +0900

    [Gold III] Title: 두 배열의 합, Time: 316 ms, Memory: 41072 KB -BaekjoonHub

--- 백준/Gold/2143. 두 배열의 합/두 배열의 합.cc
+++ 백준/Gold/2143. 두 배열의 합/두 배열의 합.cc
@@ -1,46 +1,68 @@
 #include <iostream>
-#include <map>
+#include <unordered_map>
 using namespace std;
-void GetInput(int arr[1000], int n)
-{
-	for (int i = 0; i < n; i++)
-		cin >> arr[i];
-}
-void GetSubSum(int arr[1000], int n, map<long, long>& m)
-{
-	for (int i = 0; i < n; i++)
-	{
-		int sum = 0;
-		for (int j = i; j < n; j++)
-		{
-			sum += arr[j];
-			auto iter = m.find(sum);
-			if (iter == m.end()) m.insert({ sum, 1 });
-			else iter->second += 1;
-		}
-	}
-}
+
+int t;
+int A[1000], B[1000];
+int n, m;
+unordered_map<long, long> mA, mB;
+
 int main()
 {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
-	map<long, long> sumA, sumB;
-	int t, n, m;
-	int A[1000], B[1000];
-	cin >> t;
-	cin >> n;
-	GetInput(A, n);
-	cin >> m;
-	GetInput(B, m);
-	GetSubSum(A, n, sumA);
-	GetSubSum(B, m, sumB);
-	long answer = 0;
-	for (auto iterA = sumA.begin(); iterA != sumA.end(); ++iterA)
-	{
-		auto iterB = sumB.find(t - iterA->first);
-		if (iterB == sumB.end())continue;
-		answer += iterA->second * iterB->second;
-	}
-	cout << answer;
-	return 0;
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    
+    cin >> t >> n;
+    for(int i =0; i < n; i++)
+        cin >> A[i];
+    cin >> m;
+    for(int i =0; i < m; i++)
+        cin >> B[i];
+        
+    for(int i =0; i < n; i++)
+    {
+        int sum = 0;
+        for(int j = i; j < n; j++)
+        {
+            sum += A[j];
+            auto iter = mA.find(sum);
+            if(iter != mA.end())
+            {
+                iter-> second += 1;
+            }
+            else
+            {
+                mA.insert({sum, 1});
+            }
+        }
+    }
+        
+    for(int i =0; i < m; i++)
+    {
+        int sum = 0;
+        for(int j = i; j < m; j++)
+        {
+            sum += B[j];
+            auto iter = mB.find(sum);
+            if(iter != mB.end())
+            {
+                iter-> second += 1;
+            }
+            else
+            {
+                mB.insert({sum, 1});
+            }
+        }
+    }    
+    
+    long answer = 0;
+    for(auto iter = mA.begin(); iter != mA.end(); iter++)
+    {
+        auto biter = mB.find(t - iter->first);
+        if(biter == mB.end())continue;
+        answer += iter->second * biter->second;
+    }
+    cout << answer;
+    return 0;
 }
