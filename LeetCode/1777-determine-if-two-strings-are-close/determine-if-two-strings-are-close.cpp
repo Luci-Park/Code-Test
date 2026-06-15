9631f515f30959db6bb7ff77efe61862c1838b7c
Sumin Park <97658764+Luci-Park@users.noreply.github.com>
2026-06-15 08:13:17 -0700

    Time: 4 ms (82.18%) | Memory: 23.5 MB (64.87%) - LeetSync

---
+++ LeetCode/1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool closeStrings(string word1, string word2) {
+        if(word1.size() != word2.size()) return false;
+        int arr1[26] = {0}, arr2[26] = {0};
+        for(int i =0; i < word1.size(); ++i)
+            arr1[word1[i] - 'a'] += 1;
+        for(int i =0; i < word2.size(); ++i)
+        {
+            int idx = word2[i] - 'a';
+            arr2[idx] += 1;
+            if(!arr1[idx]) return false;
+        }
+        sort(arr1, arr1 + 26);        
+        sort(arr2, arr2 + 26);
+        for(int i = 0; i < 26; i++)
+        {
+            if(arr1[i] != arr2[i]) return false;
+        }
+        return true;
+    }
+};
