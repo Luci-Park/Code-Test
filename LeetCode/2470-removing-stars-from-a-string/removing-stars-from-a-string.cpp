91e62d7686f3f2e07f01b57dccc1197737cc3ffc
Sumin Park <97658764+Luci-Park@users.noreply.github.com>
2026-06-18 18:43:00 -0700

    Time: 24 ms (28.17%) | Memory: 28.6 MB (47.12%) - LeetSync

---
+++ LeetCode/2470-removing-stars-from-a-string/removing-stars-from-a-string.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string removeStars(string s) {
+        string ans = "";
+        for(int i =0; i < s.size(); i++)
+        {
+            if(s[i] != '*')
+                ans += s[i];
+            else if(!ans.empty())
+                ans.pop_back();
+        }
+        return ans;
+    }
+};
